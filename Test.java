b90253bc52ffef1a1f3b13da065ce00eaf28deea
kmjcbe <86522673+kmjcbe@users.noreply.github.com>
2021-06-27 07:21:20 +0530

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,5 +1,20 @@
 class Test {
-Public Static void main (String args[]) {
-System.out.println("test");
-}
+	
+	private void Test () {
+		Test a1 = new Test();
+		a1.display();
+	}
+	
+	private void display() {
+		System.out.println("hey im here");
+	}
+	
+	
+	public static void main (String args[]) {
+		
+		Test a = new Test();
+		a.display();
+		
+		System.out.println("test");
+	}
 }
